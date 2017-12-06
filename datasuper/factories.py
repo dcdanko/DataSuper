9c1c61e1814da225a6d710de726d49caac64e210
David Danko <dcdanko@gmail.com>
2017-12-06 17:17:27 -0500

    more factories

--- datasuper/factories.py
+++ datasuper/factories.py
@@ -1,36 +1,51 @@
 from datasuper.database import *
 
 
+def makeSample(repo, name, sampleType, modify=False):
+    sample = SampleRecord(repo,
+                          name=name,
+                          sample_type=sampleType)
+    sample = sample.save(modify=modify)
+    return sample
+
+
 def getOrMakeSample(repo, name, sampleType):
     try:
         sample = repo.sampleTable.get(name)
     except KeyError:
-        sample = SampleRecord(repo,
-                              name=name,
-                              sample_type=sampleType)
-        sample = sample.save()
+        sample = makeSample(repo, name, sampleType)
     return sample
+
+
+def makeFile(repo, name, filepath, filetype, modify=False):
+    fr = ds.FileRecord(repo,
+                       name=name,
+                       filepath=filepath,
+                       file_type=filetype)
+    fr.save(modify=modify)
+    return fr
 
 
 def getOrMakeFile(repo, name, filepath, filetype):
     try:
         fr = repo.fileTable.get(name)
     except KeyError:
-        fr = ds.FileRecord(repo,
-                           name=name,
-                           filepath=filepath,
-                           file_type=filetype)
-        fr.save()
+        fr = makeFile(repo, name, filepath, filetype)
     return fr
+
+
+def makeResult(repo, name, resultType, fileRecs, modify=False):
+    result = ds.ResultRecord(repo,
+                             name=name,
+                             result_type=resultType,
+                             file_records=fileRecs)
+    result = result.save()
+    return result
 
 
 def getOrMakeResult(repo, name, resultType, fileRecs):
     try:
         result = repo.resultTable.get(rname)
     except KeyError:
-        result = ds.ResultRecord(repo,
-                                 name=name,
-                                 result_type=resultType,
-                                 file_records=fileRecs)
-        result = result.save()
+        result = makeResult(repo, name, resultType, fileRecs)
     return result

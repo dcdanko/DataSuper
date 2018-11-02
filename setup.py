7af824b355ff4e81808c8c66261de4abebdb825c
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-11-02 09:40:07 -0400

    Centralize version location. Tidy setup.py. Add verify command.

--- setup.py
+++ setup.py
@@ -1,27 +1,58 @@
-import setuptools
+"""DataSuper - Organization for scientific projects."""
 
-setuptools.setup(
+import os
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+from datasuper.version import __version__
+
+
+dependencies = [
+    'click==6.7',
+    'py-archy==1.0.1',
+    'PyYAML==3.12',
+    'tinydb==3.5.0',
+    'ujson==1.35',
+    'yaml_backed_structs==0.9.0',
+]
+
+
+def readme():
+    """Print long description."""
+    with open('README.rst') as readme_file:
+        return readme_file.read()
+
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version."""
+
+    description = 'Verify that the git tag matches our version.'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != 'v{0}'.format(__version__):
+            info = 'Git tag: {0} does not match the version of this app: {1}'
+            info = info.format(tag, __version__)
+            sys.exit(info)
+
+
+setup(
     name="DataSuper",
-    version="0.9.0",
+    version=__version__,
     url="https://github.com/dcdanko/DataSuper",
 
     author="David C. Danko",
     author_email="dcdanko@gmail.com",
 
     description="Organization for scientific projects.",
-    long_description=open('README.rst').read(),
+    long_description=readme(),
 
-    packages=['datasuper'],
-    package_dir={'datasuper': 'datasuper'},
+    packages=find_packages(exclude=['tests']),
 
-    install_requires=[
-        'click==6.7',
-        'py-archy==1.0.1',
-        'PyYAML==3.12',
-        'tinydb==3.5.0',
-        'ujson==1.35',
-        'yaml_backed_structs==0.9.0',
-    ],
+    install_requires=dependencies,
 
     entry_points={
         'console_scripts': [
@@ -30,10 +61,15 @@ setuptools.setup(
     },
 
     classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
+
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    },
 )
